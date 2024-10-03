c0e207b0d16a5324e656ccec7020ebaece927b9e
haris shafi <146629838+harisshafi10@users.noreply.github.com>
2024-10-03 16:47:05 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     <header className="sticky top-0 z-50 w-full border-b border-gray-800 bg-gray-900/95 backdrop-blur
      supports-[backdrop-filter]:bg-gray-900/60">
       <div className="container flex h-14 items-center">
-        <div className="mr-4 hidden md:flex">
+        <div className="mr-4 md:flex">
           <Link className="mr-6 flex items-center space-x-2" href="/">
-            <span className="hidden font-bold sm:inline-block text-emerald-400">WANDERING KASHMIR</span>
+            <span className=" font-bold sm:inline-block text-emerald-400">WANDERING KASHMIR</span>
           </Link>
           <nav className="flex items-center space-x-6 text-sm font-medium">
             <Link className="transition-colors hover:text-emerald-400 text-gray-400" href="#buy">
